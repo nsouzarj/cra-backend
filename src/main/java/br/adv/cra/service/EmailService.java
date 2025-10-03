210268019a2a0da731bddc4c6bf6815fdf1ca957
Nelson Seixas <nsouzarj@outlook.com>
2025-10-03 17:47:04 -0300

    wwrwer

--- src/main/java/br/adv/cra/service/EmailService.java
+++ src/main/java/br/adv/cra/service/EmailService.java
@@ -1,18 +1,16 @@
 package br.adv.cra.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
-
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
 
     @Autowired
+
     private JavaMailSender mailSender;
 
     /**
